d91a67404af08d27a58759e7f42ff325430ade7a
Abhijeet Singh <abhijeet15004@iiitd.ac.in>
2018-03-31 11:53:36 +0530

    Key change

--- app/src/main/java/com/example/sagar/buskaro/Route_Description.java
+++ app/src/main/java/com/example/sagar/buskaro/Route_Description.java
@@ -81,7 +81,7 @@ public class Route_Description extends AppCompatActivity implements OnMapReadyCa
     List<Routedesc_getters> routedescgettersList;
     ImageView backbutton;
     private static final String DIRECTION_URL_API = "https://maps.googleapis.com/maps/api/directions/json?";
-    private static final String GOOGLE_API_KEY = "AIzaSyDnwLF2-WfK8cVZt9OoDYJ9Y8kspXhEHfI";
+    private static final String GOOGLE_API_KEY = "AIzaSyBFjK8UInAeNGfhx8attCH8UNY6xzNjuwU";
     private DirectionFinderListener listener;
     private List<Marker> originMarkers = new ArrayList<>();
     private List<Marker> destinationMarkers = new ArrayList<>();
